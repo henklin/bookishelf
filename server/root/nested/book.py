16d9e0cfb45c30d5100aced9a790619392196162
U-HENRIK-LAPTOP\Henrik <henrik.linde@live.se>
2016-11-23 11:43:53 +0100

    added some HTML code

--- server/root/nested/book.py
+++ server/root/nested/book.py
@@ -27,7 +27,64 @@ class Book:
         cur = conn1.cursor()
         cur.execute("SELECT title FROM book where id=%s", (bookid,))
         name = cur.fetchone()
-        return name
+        return """
+        <html>
+
+<head>
+<title>Bookishelf</title>
+</head>
+
+<body background="Profile.jpg" text=#D6EAF8>
+
+    
+    <a href="Homepage.html">
+    <img src="http://e2a.solapo.com/img/i22639.jpg" height="50" width="100">
+    </a>
+
+    <h1>
+    <font face="Century Gothic" color="#FCF3CF">
+    &nbsp LOGIN TO ENTER THE FUTURE
+    </font>
+    </h1>
+    <font size=5 face="Consolas" color="#FADBD8">
+    <hr width=50% align="left">
+    <br><br><br>
+
+    <font color="#EBF5FB"><p align="center">Enter your details to Login</font>
+
+    <form action="http://localhost:8080/api/books/" method="post">
+    <table align="center">
+
+    <tr><td>
+    User-ID:</td><td align="left"><input type="text" name="bookid" maxlength="32" size="16">
+    </td></tr>
+
+    <tr><td>
+    Password:</td><td align="left"><input type="password" name="userid">
+    </td></tr>
+
+    <tr><td align="center"><input type="submit" value="Login"></td></tr>
+
+    </table>
+
+    </form>
+
+    <br><br>
+    <p align="center"><font face="Century Gothic" color="#2E86C1">
+    Not a member yet?
+    </font>
+    <a href=Register.html><font color="#EBF5FB">Sign Up</font></a>
+
+    </font>
+</body>
+<br><br><br>
+<font face="Century Gothic" color="#1A5276">
+<hr align="center" width="50%">
+<p align="center">&copy2016&nbsp Bookishelf.com
+</font>
+
+</html>
+"""
         
     def POST(self, id, title, qty, price):
         
