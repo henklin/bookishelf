e74e859ec97ebf7f65cfe35c225a0b5426260a3a
HernandezAlejandro <alepe1231@hotmail.com>
2016-12-13 14:40:48 +0100

    fixed changes in book that got erased when henrik pushed

--- server/root/nested/book.py
+++ server/root/nested/book.py
@@ -23,33 +23,41 @@ class Book:
     
     exposed = True
     
-    def GET():
+    def GET(self, bookid):
         conn1 = pymysql.connect(host='localhost', port=3306, user='root', passwd='admin', db='mydb', autocommit=True)
         cur = conn1.cursor()
-        cur.execute("SELECT * FROM book")
-    
-                    # where id=%s", (bookid,)
+        cur.execute("SELECT * FROM book WHERE id=%s", (bookid))
         name = cur.fetchone()
         return str(name)
     
     
     
-    def POST(self, id, title, qty, price, image):
+    def POST(self, title, qty, price, image, category, description):
         
         conn1 = pymysql.connect(host='localhost', port=3306, user='root', passwd='admin', db='mydb', autocommit=True)
         
         cur = conn1.cursor()
         
-        cur.execute("INSERT INTO book VALUES(%s, %s, %s, %s, 0, %s)", (id, title, qty, price,image))
+        cur.execute("INSERT INTO book (title, qty, price, nrSold, image, category, description) VALUES(%s, %s, %s, 0, %s, %s, %s)", (title, qty, price, image, category, description))
         #cur.commit()
         cur.close
         return ('Success!')
+    
+    
+    def PUT(self, bookid):
+        
+        conn1 =pymysql.connect(host='localhost', port=3306, user='root', passwd='admin', db='mydb', autocommit=True)
+        
+        cur = conn1.cursor()
+        cur.execute("DELETE FROM book WHERE id=%s", (bookid))
+        name = cur.fetchone()
+        return str('Success!')
 
 # return ('The book: %s The user: %s' % (dbquery1, dbquery2))
 
-                    
-                    
-                    
-                    
-                    
-                
+
+
+
+
+
+
