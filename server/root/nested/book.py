246db837eae221b25770d0ce024fbb2522bba00f
HernandezAlejandro <alepe1231@hotmail.com>
2016-12-15 08:39:29 +0100

    Updated book and QT

--- server/root/nested/book.py
+++ server/root/nested/book.py
@@ -1,15 +1,10 @@
 '''
-    Created on 21 nov. 2016
-    
-    @author: Henrik
-    '''
-
-
-'''
     Created on 18 nov. 2016
     
     @author: Henrik
-    '''
+    @author: Alejandro P. Hernandez
+    
+'''
 
 import cherrypy
 import pymysql
@@ -42,22 +37,35 @@ class Book:
         #cur.commit()
         cur.close
         return ('Success!')
-    
-    
-    def PUT(self, bookid):
+
+
+    def PUT(self, bookid, valueChange, nrChange):
         
         conn1 =pymysql.connect(host='localhost', port=3306, user='root', passwd='admin', db='mydb', autocommit=True)
-        
+            
         cur = conn1.cursor()
-        cur.execute("DELETE FROM book WHERE id=%s", (bookid))
-        name = cur.fetchone()
+        
+        if nrChange == '1':
+            cur.execute ("""UPDATE book SET title = %s WHERE id = %s """ , (valueChange,bookid))
+        elif nrChange == '3':
+            cur.execute ("""UPDATE book SET price = %s WHERE id = %s """ , (valueChange,bookid))
+        elif nrChange == '2':
+            cur.execute ("""UPDATE book SET qty = %s WHERE id = %s """ , (valueChange,bookid))
+        elif nrChange == '5':
+            cur.execute ("""UPDATE book SET image = %s WHERE id = %s """ , (valueChange,bookid))
+        elif nrChange == '6':
+            cur.execute ("""UPDATE book SET category = %s WHERE id = %s """ , (valueChange,bookid))
+        elif nrChange == '7':
+            cur.execute ("""UPDATE book SET description = %s WHERE id = %s """ , (valueChange,bookid))
+
+        cur.close
         return str('Success!')
 
 # return ('The book: %s The user: %s' % (dbquery1, dbquery2))
 
-
-
-
-
-
-
+                    
+                    
+                    
+                    
+                    
+                
