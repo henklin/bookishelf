6431fdcbf18018a9a3770f072fc86f6230ed32f4
HernandezAlejandro <alepe1231@hotmail.com>
2016-12-20 08:50:36 +0100

    Changes in bookpage, add a buttom

--- server/root/nested/bookPage.py
+++ server/root/nested/bookPage.py
@@ -53,63 +53,67 @@ class BookPage:
         
         
         return("""<html>
-            
-            <head>
-            <title>Book page</title>
-            </head>
-            
-            <div align="left">
-            <a href="Homepage.html">
-            <img src="http://images.clipartpanda.com/embryo-clipart-book17.png" height="50" width="100">
-            </a>
-            
-            <div align="right">
-            <form action="http://127.0.0.1:8080/api/shoppingCart" method="post">
-            <button>
-            <input type="hidden" name="userid" value="%s">
-            Shopping Cart<br><img src="https://www.iconexperience.com/_img/g_collection_png/standard/512x512/shopping_cart.png" alt="Shopping Cart" height="150" width="150">
-            </button>
-            </form>
-            </div>
-            
-            
-            <body background="http://wallpaperus.org/wallpapers/03/122/books-1920x1080-wallpaper-1711426.jpg" text=#D6EAF8>
-            
-            
-            <h1 align="center"><font face="Century Gothic" color="#EBF5FB">%s</font></h1>
-            <font size=5 face="Consolas" color="#EBF5FB">
-            <hr width=30>
-            
-            <table align="center" cellpadding=55>
-            <tr>
-            <td><img height="500" width="400" src="%s" alt="book1"></td>
-            <td>
-            <font color="#EBF5FB" size=5 face="Source Sans Pro">Details<br><br>%s<br>
-            <font size=3>Author-TBA</font><br>
-            Price-%s kr<br>
-            %s<br>
-            </font><br>
-            <form action="credit.html">
-            <input type = "submit" value="Add to Cart" name="cart">
-            </form>
-            </td>
-            </tr>
-            <tr><td>
-            <button onClick="" value="Go to Book circle"s>Go to Book Circle</button>
-            </td></tr></table>
-            <font face="Century Gothic" color="#1A5276">
-            <hr align="center" width="50">
-            <p align="center">&copy2016&nbsp Bookishelf.com
-            </font>
-            </body>
-            
-            
-            </html>
-""" % (userid, bookTitle[0], bookImage[0], bookDesc[0], bookPrice[0], qtyString))
+
+<head>
+<title>Book page</title>
+</head>
+
+    <div align="left">
+    <a href="Homepage.html">
+    <img src="http://images.clipartpanda.com/embryo-clipart-book17.png" height="50" width="100">
+    </a>
+
+    <div align="right">
+    <form action="http://127.0.0.1:8080/api/shoppingCart" method="post">
+    <button>
+    <input type="hidden" name="userid" value="%s">
+    Shopping Cart<br><img src="https://www.iconexperience.com/_img/g_collection_png/standard/512x512/shopping_cart.png" alt="Shopping Cart" height="150" width="150">
+    </button>
+    </form>
+    </div>
+    
+
+<body background="http://wallpaperus.org/wallpapers/03/122/books-1920x1080-wallpaper-1711426.jpg" text=#D6EAF8>
 
 
+<h1 align="center"><font face="Century Gothic" color="#EBF5FB">%s</font></h1>
+<font size=5 face="Consolas" color="#EBF5FB">
+<hr width=30>
+
+<table align="center" cellpadding=55>
+<tr>
+<td><img height="500" width="400" src="%s" alt="book1"></td>
+<td>
+<font color="#EBF5FB" size=5 face="Source Sans Pro">Details<br><br>%s<br>
+<font size=3>Author-TBA</font><br>
+Price-%s kr<br>
+%s<br>
+</font><br>
+<form action="credit.html">
+<input type="submit" value="Add to Cart" name="cart">
+</form>
+</td>
+</tr>
+<tr><td>
+<form action="credit.html">
+<input type="hidden" value="%s" name="bookid">
+<input type="submit" value="Go to Book Circle">
+</form>
+</td></tr>
+</table>
+
+<font face="Century Gothic" color="#1A5276">
+<hr align="center" width="50">
+<p align="center">&copy2016&nbsp Bookishelf.com
+</font>
+</body>
 
 
+        </html>""" % (userid, bookTitle[0], bookImage[0], bookDesc[0], bookPrice[0], qtyString, bookid))
+
+    
+    
+    
     def GET(self, bookid):
         
         conn1 = pymysql.connect(host='localhost', port=3306, user='root', passwd='admin', db='mydb', autocommit=True)
